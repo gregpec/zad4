dad97fd977bb295227137d07d3964fcb75d3a2af
gregpec <gregpec@yahoo.com>
2023-12-13 03:01:49 +0100

    zadanie nr 4

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.ComponentModel.Design;
+using System.Xml.Linq;
+string plec = "Kobieta";
+string name = "Ewa";
+int age = 17;
+
+if (plec == ("Kobieta") && name == "Ewa " && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else
+{
+    if (name != ("Ewa") && age > 30)
+    {
+
+        Console.WriteLine("Kobieta powyżej 30 lat");
+    }
+    else
+    {
+        if (plec != ("Kobieta") && age < 18)
+        {
+            Console.WriteLine("Niepelnoletni mezczyzna");
+        }
+        else
+        {
+            Console.WriteLine("Niespelniony zaden z warunków");
+        }
+    }
+}
